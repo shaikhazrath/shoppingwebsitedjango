4d3157fcba566a1937e67d8f2ace2758ffffe734
shaikhazrath <108566186+shaikhazrath@users.noreply.github.com>
2023-01-13 14:13:58 +0000

    Update settings.py

--- sixtharena/settings.py
+++ sixtharena/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.55.108','web-production-406b.up.railway.app']
 
